39f10cf720102be90768127475938fff48c1a9e9
mbemah85 <mbemah85@gmail.com>
2022-10-27 08:25:05 -0700

    task 0 redp

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,29 +1,14 @@
 #include "main.h"
 
 /**
- * _strbcat - Joining two strings with n bytes
+ * _strncat - Joining two strings with n bytes
  * @dest: copy to
  * @src: copy from
  * @n: number of char to be copied
- * Return: dest
+ * Return: string
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	int j;
-
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	j = 0;
-	while (j < n && src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
+	strncat(dest, *src, n);
 	return (dest);
 }
