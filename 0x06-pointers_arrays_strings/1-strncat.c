def73bc1fa73a152a81ef6d513715066c03b0cf1
mbemah85 <mbemah85@gmail.com>
2022-10-27 08:42:31 -0700

    worked on task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,15 +1,30 @@
+
 #include "main.h"
-#include <string.h>
 
 /**
- * _strncat -> function to append some charx
- * @dest: first param
- * @src: second param
- * @n: third param
- * Return: string
+ * _strncat - Joining two strings with n bytes
+ * @dest: copy to
+ * @src: copy from
+ * @n: number of char to be copied
+ * Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	strncat(dest, *src, n);
-	return (dest);
+        int i;
+        int j;
+
+        i = 0;e
+        while (dest[i] != '\0')
+        {
+                i++;
+        }
+        j = 0;
+        while (j < n && src[j] != '\0')
+        {
+                dest[i] = src[j];
+                i++;
+                j++;
+        }
+        dest[i] = '\0';
+        return (dest);
 }
